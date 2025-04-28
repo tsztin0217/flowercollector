6796d6ba151fb7ef81bbdef323e71901950eea32
tsztin0217 <tsztin0217@gmail.com>
2025-04-27 22:48:01 -0400

    Add create, update, and delete path for flower

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('flowers/', views.flower_index, name='flower-index'),
     path('flowers/<int:flower_id>/', views.flower_detail, name='flower-detail'),
+    path('flowers/create/', views.FlowerCreate.as_view(), name='flower-create'),
+    path('flowers/<int:pk>/update/', views.FlowerUpdate.as_view(), name='flower-update'),
+    path('flowers/<int:pk>/delete/', views.FlowerDelete.as_view(), name='flower-delete'),
 ]
