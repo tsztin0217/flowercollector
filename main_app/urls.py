dd3f43f635dae2351d72f5803d9d3a94a631e2f5
tsztin0217 <tsztin0217@gmail.com>
2025-04-28 01:34:56 -0400

    Add path for watering and pots

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,16 @@ urlpatterns = [
     path('flowers/create/', views.FlowerCreate.as_view(), name='flower-create'),
     path('flowers/<int:pk>/update/', views.FlowerUpdate.as_view(), name='flower-update'),
     path('flowers/<int:pk>/delete/', views.FlowerDelete.as_view(), name='flower-delete'),
+    path(
+        'flowers/<int:flower_id>/add-watering/', 
+        views.add_watering, 
+        name='add-watering'
+    ),
+    path('pots/create/', views.PotCreate.as_view(), name='pot-create'),
+    path('pots/<int:pk>/', views.PotDetail.as_view(), name='pot-detail'),
+    path('pots/', views.PotList.as_view(), name='pot-index'),
+    path('pots/<int:pk>/update/', views.PotUpdate.as_view(), name='pot-update'),
+    path('pots/<int:pk>/delete/', views.PotDelete.as_view(), name='pot-delete'),
+    path('flowers/<int:flower_id>/associate-pot/<int:pot_id>/', views.associate_pot, name='associate-pot'),
+    path('flowers/<int:flower_id>/remove-pot/<int:pot_id>/', views.remove_pot, name='remove-pot'),
 ]
