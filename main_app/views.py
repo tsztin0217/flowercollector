923f215c9704c59b3eb82ad8068023bed92b3d1d
tsztin0217 <tsztin0217@gmail.com>
2025-04-27 22:48:21 -0400

    Add Create, Update and Delete View

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db import models
 from .models import Flower
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 def about(request):
     return render(request, 'about.html')
@@ -29,3 +30,15 @@ def flower_index(request):
 def flower_detail(request, flower_id):
     flower = Flower.objects.get(id=flower_id)
     return render(request, 'flowers/detail.html', {'flower': flower})
+
+class FlowerCreate(CreateView):
+    model = Flower
+    fields = ['name', 'color']
+
+class FlowerUpdate(UpdateView):
+    model = Flower
+    fields = ['color']
+
+class FlowerDelete(DeleteView):
+    model = Flower
+    success_url = '/flowers/'
