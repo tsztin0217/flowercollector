8b07c0b8685aea4485e1d45451d67744bb302c07
tsztin0217 <tsztin0217@gmail.com>
2025-04-27 21:47:38 -0400

    Add flower detail path

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('flowers/', views.flower_index, name='flower-index'),
+    path('flowers/<int:flower_id>/', views.flower_detail, name='flower-detail'),
 ]
