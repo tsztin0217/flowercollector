e0c0c0ee131ce17cb1e3ed6590839332c8ce5ce2
tsztin0217 <tsztin0217@gmail.com>
2025-04-27 20:03:59 -0400

    Create temporary flower class to generate some data

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 
-# Import HttpResponse to send text-based responses
-from django.http import HttpResponse
-
-# Define the home view function
-def home(request):
-    # Send a simple HTML response
-    return HttpResponse('<h1>Hello user')
-
-# main_app/views.py
-
 def about(request):
     return render(request, 'about.html')
 
+
+class Flower:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+# Create a list of Flower instances
+flowers = [
+    Flower('Rose', 'red'),
+    Flower('Lily', 'yellow'),
+    Flower('Poppy', 'red'),
+    Flower('Orchid', 'purple')
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def flower_index(request):
+    return render(request, 'flowers/index.html', {'flowers': flowers})
