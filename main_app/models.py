e198615dda9714e79345c54c80457c20c6ed58ad
tsztin0217 <tsztin0217@gmail.com>
2025-04-27 22:47:43 -0400

    Create a watering model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.urls import reverse
+
+TIMES = (
+    ('M', 'Morning'),
+    ('N', 'Noon'),
+    ('E', 'Evening')
+)
 
 # Create your models here.
 class Flower(models.Model):
@@ -7,3 +14,21 @@ class Flower(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("flower_detail", kwargs={"flower_id": self.id})
+    
+
+
+class Watering(models.Model):
+    date = models.DateField('Watering date')
+    time = models.CharField(
+        max_length=1,
+        choices=TIMES,
+        default=TIMES[0][0]
+    )
+    
+    flower = models.ForeignKey(Flower, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_time_display()} on {self.date}"
